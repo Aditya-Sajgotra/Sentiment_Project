4d17777e88bf02bb3e6a1baf3bf67298449fa968
Aditya-Sajgotra <anukhajuria089@gmail.com>
2026-07-08 23:10:27 +0530

    restore flat root repository layout for clean local testing

--- app/app.py
+++ app/app.py
@@ -5,15 +5,14 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from db.main import input_data
-from model_func.predict import predict,MODEL_VERSION,get_model
-
-
+from model_func.predict import predict, MODEL_VERSION, get_model
 
 app = FastAPI()
 
 
 class Input(BaseModel):
     """Basic structure of how an input should look like"""
+
     id: Annotated[
         Optional[int],
         Field(description="Id for identification of the input", examples=[1, 2]),
@@ -27,9 +26,9 @@ class Input(BaseModel):
 def get_response(data: Input) -> JSONResponse:
     """It returns a response corresponding to user's input"""
     output = predict(data.input)
-    output_label = output[0]['label']
+    output_label = output[0]["label"]
     output_score = output[0]["score"]
-    input_data(input = data.input,sentiment_output = output_label,confidence=output_score)
+    input_data(input=data.input, sentiment_output=output_label, confidence=output_score)
     return JSONResponse(
         status_code=200,
         content={"sentiment": output_label, "confidence": output_score},
