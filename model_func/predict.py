213375cda185a93f9326d95511ebacf9f1a03924
Aditya-Sajgotra <anukhajuria089@gmail.com>
2026-07-08 21:56:29 +0530

    implement lazy loading function for model signature

--- model_func/predict.py
+++ model_func/predict.py
@@ -1,9 +1,12 @@
 """Module for loading model and getting predictions from it"""
 from transformers import pipeline
-
-model = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 MODEL_VERSION = "1.0.0"
 
+def get_model():
+
+    model = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+    return model
 
 def predict(input: str) -> dict:
+    model = get_model()
     return model.predict(input)
